03cb7d61a2eb52121c111c4877d7f43802b5e32b
Jorge Lucas <jorgegrlucas@gmail.com>
2023-03-30 13:05:28 -0300

    Adding JPA

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,5 +1,8 @@
 package med.voll.api.endereco;
 
-public record DadosEndereco(String logradouro, String bairro, String cep, String uf, String complemento, String numero) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(@NotBlank String logradouro, @NotBlank String bairro, @NotBlank @Pattern(regexp = "\\d{8}") String cep, @NotBlank String uf, String complemento, String numero, @NotBlank String cidade) {
 
 }
